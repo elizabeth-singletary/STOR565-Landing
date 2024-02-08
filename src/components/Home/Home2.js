28a86a5c50914fcaee0ed43dca942fcf24132303
elizabeth-singletary <103393032+elizabeth-singletary@users.noreply.github.com>
2024-02-08 11:22:14 -0500

    text changes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -14,7 +14,7 @@ function Home2() {
               Our <span className="purple"> Goal </span> 
             </h1>
             <p className="home-about-body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Our project aims to classify/tag abusive and hateful language from textual data. We will explore several techniques to distinguish the best model for our applications, techniques including: PCA, NLP, logistic regression, decision trees, etc. Our ultimate goal would be to create the backbone of an application that could be used to ensure safe consumption of media for children. 
                       
             </p>
           </div>
