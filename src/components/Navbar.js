f5d566f57420d87b287ef2c80c53f0ccb6a07611
elizabeth-singletary <103393032+elizabeth-singletary@users.noreply.github.com>
2024-05-03 22:42:18 -0400

    fixed slashees

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -77,7 +77,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="public\MLFs_Final_Paper.pdf"
+                href="public/MLFs_Final_Paper.pdf"
                 target="_blank"
                 onClick={() => updateExpanded(false)}
               >
